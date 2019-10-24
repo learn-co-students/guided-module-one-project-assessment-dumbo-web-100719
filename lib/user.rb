0ea22cdd9faf0ead29c9f46a50a72e49e2848ecd
Hope <hopegiometti@gmail.com>
2019-10-24 14:05:13 -0400

    fixed update

--- lib/user.rb
+++ lib/user.rb
@@ -13,39 +13,15 @@ class User < ActiveRecord::Base
 
     def pick_a_career
         @new_plan = self.plans.create
-        # binding.pry
-        selection = prompt.select("Select a career from this list:", [
-            { name: "developer", value: 1 },
-            { name: "teacher", value: 2 },
-            { name: "nurse", value: 3 },
-            { name: "doctor", value: 4 },
-            { name: "pilot", value: 5 } 
-        ])
-        if selection == 1
-            @new_plan.career = Career.all.find {|career| career.name == "Software Developer"}
-        elsif selection == 2
-            @new_plan.career = Career.all.find {|career| career.name == "Teacher"}
-        elsif selection == 3
-            @new_plan.career = Career.all.find {|career| career.name == "Nurse"}
-        elsif selection == 4
-            @new_plan.career = Career.all.find {|career| career.name == "Doctor"}
-        elsif selection == 5
-            @new_plan.career = Career.all.find {|career| career.name == "Pilot"}
+        all_careers = Career.all.map {|career| {name: career.name, value: career.id}}
+        selection = prompt.select("Select a career from this list:", all_careers)
+        Career.all.each do |career|
+            if career.id == selection
+                @new_plan.career = career
+            end
         end
         puts "Cool, your new career is #{@new_plan.career.name} and your new salary is $#{@new_plan.career.salary} a year!"
     end
-
-    # def pick_a_lifestyle
-    #     selection = prompt.select("", [
-    #         { name: "beach bum", value: 1 },
-    #         { name: "Coastal Elite", value: 2 },
-    #         { name: "Small city, big town", value: 3 },
-    #         { name: "hipster", value: 4 }
-    # ])
-    # if selection == 1
-    #     @new_plan.place.lifestyle 
-
-    # end
 
     def pick_a_place
         selection = prompt.select("Let's look at some places:", [
@@ -99,66 +75,26 @@ class User < ActiveRecord::Base
                 end
             elsif selection == 3
                 Place.all.map do |place|
-                    puts "Welcome to #{place.name}! The median income is #{place.median_income}."
+                    puts "#{place.name} has a median income of #{place.median_income}."
                 end
             end
-                
-            selection = prompt.select("Alright, let's pick a place to live!", [
-                { name: "Miami", value: 1 },
-                { name: "Los Angeles", value: 2 },
-                { name: "Boston", value: 3 },
-                { name: "Seattle", value: 4 },
-                { name: "Charlotte", value: 5 },
-                { name: "Pittsburgh", value: 6 },
-                { name: "Kansas City", value: 7 },
-                { name: "Cincinnati", value: 8 },
-                { name: "San Diego", value: 9 },
-                { name: "San Antonio", value: 10 },
-                { name: "Austin", value: 11 },
-                { name: "Manhattan", value: 12 },
-                { name: "Brooklyn", value: 13 },
-                { name: "Denver", value: 14 }
-            ])
-            if selection == 1
-                @new_plan.place = Place.all.find {|place| place.name == "Miami, Florida"}
-            elsif selection == 2
-                @new_plan.place = Place.all.find {|place| place.name == "Los Angeles, California"}
-            elsif selection == 3
-                @new_plan.place = Place.all.find {|place| place.name == "Boston, Massachusetts"}
-            elsif selection == 4
-                @new_plan.place = Place.all.find {|place| place.name == "Seattle, Washington"}
-            elsif selection == 5
-                @new_plan.place = Place.all.find {|place| place.name == "Charlotte, North Carolina"}
-            elsif selection == 6
-                @new_plan.place = Place.all.find {|place| place.name == "Pittsburgh, Pennsylvania"}
-            elsif selection == 7
-                @new_plan.place = Place.all.find {|place| place.name == "Kansas City, Missouri"}
-            elsif selection == 8
-                @new_plan.place = Place.all.find {|place| place.name == "Cincinnati, Ohio"}
-            elsif selection == 9
-                @new_plan.place = Place.all.find {|place| place.name == "San Diego, California"}
-            elsif selection == 10
-                @new_plan.place = Place.all.find {|place| place.name == "San Antonio, Texas"}
-            elsif selection == 11
-                @new_plan.place = Place.all.find {|place| place.name == "Austin, Texas"}
-            elsif selection == 12
-                @new_plan.place = Place.all.find {|place| place.name == "Manhattan, New York"}
-            elsif selection == 13
-                @new_plan.place = Place.all.find {|place| place.name == "Brooklyn, New York"}
-            elsif selection == 14
-                @new_plan.place = Place.all.find {|place| place.name == "Denver, Colorado"}
+
+            all_places = Place.all.map {|place| {name: place.name, value: place.id}}
+            selection = prompt.select("Alight, let's pick a place to live!", all_places)
+            Place.all.each do |place|
+                if selection == place.id
+                    @new_plan.place = place
+                end
             end
     end
 
     def make_a_new_plan
         @new_plan.save
-        # binding.pry
         puts "Awesome! Your new life plan is to move to #{@new_plan.place.name} and work as a #{@new_plan.career.name} and make $#{@new_plan.career.salary} per year!"
         puts "Sounds like a great life!" 
     end
 
     def update_plan
-        # binding.pry
         array_of_plans = self.plans.map {|plan| {name: "#{plan.career.name} in #{plan.place.name}", value: plan.id}}
         selection = prompt.select("Please pick a plan to change:", array_of_plans.push({name: 'Nevermind', value: -1}))
         if selection != -1
@@ -172,15 +108,15 @@ class User < ActiveRecord::Base
                         { name: "pilot", value: 5 } 
                     ])
                 if selection == 1
-                    plan.career = Career.all.find {|career| career.name == "Software Developer"}
+                    plan.career = Career.all.detect {|career| career.name == "Software Developer"}
                 elsif selection == 2
-                    plan.career = Career.all.find {|career| career.name == "Teacher"}
+                    plan.career = Career.all.detect {|career| career.name == "Teacher"}
                 elsif selection == 3
-                    plan.career = Career.all.find {|career| career.name == "Nurse"}
+                    plan.career = Career.all.detect {|career| career.name == "Nurse"}
                 elsif selection == 4
-                    plan.career = Career.all.find {|career| career.name == "Doctor"}
+                    plan.career = Career.all.detect {|career| career.name == "Doctor"}
                 elsif selection == 5
-                    plan.career = Career.all.find {|career| career.name == "Pilot"}
+                    plan.career = Career.all.detect {|career| career.name == "Pilot"}
                 end
                 puts "Cool, your new career is #{plan.career.name} and your new salary is $#{plan.career.salary} a year!"
                 plan.save
@@ -192,47 +128,6 @@ class User < ActiveRecord::Base
         end
          
     end
-
-
-    # def update_plan
-    #     array_of_plans = self.plans.map {|plan| {name: plan.place.name, value: plan.id}}
-    #     selection = prompt.select("Please pick a plan to change:", array_of_plans)
-    #     binding.pry
-    #     if selection == 
-    #     # binding.pry
-    # #   array=self.plans.map |plan| {name: plan.name, value: plan.id}
-    #     # puts "I'm sorry you aren't happy with all of your plans!"
-    #     # selection = prompt.select("Please pick a plan to change:", [
-    #     #    array_of_plans
-    #     # ])
-    #     # binding.pry
-    #     # if selection == array_of_plans
-        
-    #     # binding.pry
-
-
-    #     # if selection == 1
-    #     #     puts "Please tell me which career you want to change"
-    #     #     change_this_career = gets.chomp
-    #     #     # binding.pry
-    #     #     career_names = self.plans.all.map {|plan| plan.career.name}
-    #     #     career_names.find do |name|
-    #     #         if name == change_this_career
-    #     #         end
-    #     #     end
-    #     #     self.plans.all
-    #     #         # if plan.career == change_this_career
-    #     #         #     plan.career = change_this_career
-    #     #         #     binding.pry
-    #     #         # end
-    #     # elsif selection == 2
-    #     #     puts "cool2"
-    #     # elsif selection == 3
-    #     #     puts "Okay! I'm glad you're happy with your life!"
-    #     # end
-
-    #     # binding.pry
-    # end
 
     def delete_plan
         array_of_plans = self.plans.map {|plan| {name: "#{plan.career.name} in #{plan.place.name}", value: plan.id}}
@@ -249,23 +144,6 @@ class User < ActiveRecord::Base
         end
 
     end
-
-    # def delete_plan
-    #     self.plans.all.each do |plan|
-    #         puts "Your plan is to move to #{plan.place.name} and work as a #{plan.career.name}!"
-    #     end
-    #     puts "These are your current plans. I'm going to delete your most recent plan!"
-    #     selection = prompt.select("Is that okay with you?", [
-    #             {name: "Yes! I hated that plan!", value: 1},
-    #             {name: "No, wait, I loved that plan!", value: 2}
-    #     ])
-    #     if selection == 1   
-    #         self.plans.all.last.delete
-    #         puts "Your plan is gone. I'm glad. It was a terrible plan."
-    #     elsif selection == 2
-    #         puts "Okay, I won't delete anything!"
-    #     end
-    # end
 
     def prompt
         @prompt ||= TTY::Prompt.new
